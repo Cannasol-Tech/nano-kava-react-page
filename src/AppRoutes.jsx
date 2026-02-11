92c9f041e018fd7b87c1bb294d677351a34a15c2
Stephen Boyett <s.boyett31@gmail.com>
2026-02-11 01:47:53 -0500

    added scrollToTop

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,5 @@
-import React, { Suspense, lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 const KavaLandingPage = lazy(() => import('./components/KavaLandingPage'));
 const FAQPage = lazy(() => import('./components/FAQPage'));
@@ -7,9 +7,18 @@ const ContactPage = lazy(() => import('./components/ContactPage'));
 const MushroomsLandingPage = lazy(() => import('./components/MushroomsLandingPage'));
 const NotFoundPage = lazy(() => import('./components/NotFoundPage'));
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function AppRoutes() {
   return (
     <Suspense fallback={<div className="min-h-screen" />}>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<KavaLandingPage />} />
         <Route path="/mushrooms" element={<MushroomsLandingPage />} />
